69f4abbabfecef4e7e01e809c3d391e8f6d614ad
joannegton <wtg131998@gmail.com>
2025-06-11 22:52:41 -0300

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    allowedHosts: ['4ec6-45-179-106-143.ngrok-free.app'],
+  },
 })
